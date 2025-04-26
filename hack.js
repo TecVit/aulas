bdce1e06dca4f80ed7cc1ceee2229e07546047fe
TecVit <156000975+TecVit@users.noreply.github.com>
2025-04-26 18:31:54 -0300

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,2 +1,4 @@
 alert("Runing 0__0")
-document.querySelector('nav.navbar').style.backgroundColor = 'red';
+
+document.querySelector('nav.navbar').style.setProperty('background-color', 'red', 'important');
+document.querySelector('body').style.setProperty('background-color', 'black', 'important');
